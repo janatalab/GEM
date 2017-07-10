d8849158975e68500c250f59a2f4cd486fcd55a2
Scottie Alexander <scottiealexander11@gmail.com>
2017-07-10 16:42:25 -0700

    Remove unneeded imports from example experiment script

--- GUI/gem_example.py
+++ GUI/gem_example.py
@@ -1,14 +1,11 @@
 from GEMGUI import GEMGUI
-import serial
-from time import time
-
 import os
 
 rootpath = "/Users/" + os.environ['USER'] + "/Documents/Arduino/"
 
 presets = {
     "serial": {"port": "/dev/cu.usbmodem1451111", "baud_rate": 115200, "timeout": 5},
-    "filename": "GEM_data_",
+    "filename": "GEM_data",
     "data_dir": rootpath,
     "hfile": rootpath + "GEM/GEM/GEMConstants.h",
     # "sound_dir": "<sound_dir>",
