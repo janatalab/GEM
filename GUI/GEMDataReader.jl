0058066d3e36a736e7d4e8a2f5025a9875c267c5
Scottie Alexander <scottiealexander11@gmail.com>
2017-07-10 16:38:55 -0700

    GEMDataReader module version 0.0.2; Minor tweaks, finish write functions

--- GUI/GEMDataReader.jl
+++ GUI/GEMDataReader.jl
@@ -2,15 +2,18 @@ module GEMDataReader
 
 import JSON
 
-const MAX_SLAVES = 0x04
+export convert_file, GEMDataFile, read_run
 
+const MAX_SLAVES = 0x04
+# ---------------------------------------------------------------------------- #
 struct GEMDataFile
     path::String
     hdr::Dict
     idx_map::Vector{UInt64}
     nrun::Int
+    nwin::Int
 end
-
+# ---------------------------------------------------------------------------- #
 struct GEMPacket
     dtp::UInt8
     window::UInt16
@@ -18,7 +21,7 @@ struct GEMPacket
     async::Vector{Int16}
     adjust::Int16
 end
-
+# ---------------------------------------------------------------------------- #
 function GEMDataFile(filepath::String)
     open(filepath, "r") do io
         hdr_length = read(io, UInt64)
@@ -30,52 +33,81 @@ function GEMDataFile(filepath::String)
         seekend(io)
         len = position(io)
 
-        return GEMDataFile(filepath, hdr, idx_map, nrun)
+        return GEMDataFile(filepath, hdr, idx_map, nrun, Int(hdr["windows"]))
     end
 end
-
+# ---------------------------------------------------------------------------- #
 function convert_file(ifile::String, ofile::String)
 
     df = GEMDataFile(ifile)
 
     open(ofile, "w") do io
+        labels = join(["\"async_$(x)\"" for x in 1:MAX_SLAVES], ", ")
+        write(io,
+            """
+            \"run\", \"window\", \"click_time\", $(labels), \"next_adjust\"
+            """
+        )
         for k = 1:df.nrun
             hdr, data = read_run(df, k)
-            # TODO: write the run to the text ofile (columns etc.)
+            if isempty(data)
+                break
+            end
+            write_run(io, hdr, data, k)
         end
     end
 end
-
+# ---------------------------------------------------------------------------- #
+# Columns: run #, window #, scheduled click, async 1-4, next_adjust
+function write_run(io::IOStream, hdr::Dict, data::Vector{GEMPacket},
+    run::Integer)
+    for k in eachindex(data)
+        write(io, "$(run), $(k), " * tostring(data[k]) * "\n")
+    end
+end
+# ---------------------------------------------------------------------------- #
+function tostring(x::GEMPacket)
+    return "$(x.click_time), " * join(x.async, ", ") * ", $(x.adjust)"
+end
+# ---------------------------------------------------------------------------- #
 function read_run(file::GEMDataFile, k::Integer=1)
     hdr, offset = read_run_header(file, k)
-    nwin = Int(hdr["windows"])
-    data = Vector{GEMPacket}(nwin)
-    open(file.path, "r") do io
-        seek(io, offset)
+    if offset > 0
+        data = Vector{GEMPacket}(file.nwin)
+        open(file.path, "r") do io
+            seek(io, offset)
 
-        for k = 1:nwin
-            data[k] = read_packet(io)
+            for k = 1:file.nwin
+                data[k] = read_packet(io)
+            end
+            return hdr, data
         end
-        return hdr, data
+    else
+        return hdr, Vector{GEMPacket}()
     end
 end
-
+# ---------------------------------------------------------------------------- #
 """
 returns the run header and the position of the start of the data block for that
 run
 """
 function read_run_header(file::GEMDataFile, k::Integer=1)
-    @assert(k > file.nrun, "Invalid run number!")
+    @assert(k <= file.nrun, "Invalid run number!")
 
     offset = file.idx_map[k]
 
-    open(file.path, "r") do io
-        seek(io, offset)
-        hdr_length = read(io, UInt64)
-        return JSON.parse(String(read(io, UInt8, hdr_length))), position(io)
+    if offset > 0
+        open(file.path, "r") do io
+            seek(io, offset)
+            hdr_length = read(io, UInt64)
+            return JSON.parse(String(read(io, UInt8, hdr_length))), position(io)
+        end
+    else
+        return Dict(), -1
     end
-end
 
+end
+# ---------------------------------------------------------------------------- #
 function read_packet(io::IOStream)
     return GEMPacket(
         read(io, UInt8),            #data-transfer-protocol id
@@ -85,3 +117,5 @@ function read_packet(io::IOStream)
         read(io, Int16)             #next adjustment factor
     )
 end
+# ---------------------------------------------------------------------------- #
+end # end module
