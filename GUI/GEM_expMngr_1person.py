c309cb52aebcc82e228bb80b9749a8323e79ff69
Janata Lab <janatalab@gmail.com>
2018-01-22 16:52:56 -0800

    Latest debug parameters

--- GUI/GEM_expMngr_1person.py
+++ GUI/GEM_expMngr_1person.py
@@ -22,15 +22,15 @@ import os
 rootpath = "/Users/" + os.environ['USER'] + "/Documents/Arduino/"
 
 presets = {
-    "serial": {"port": "/dev/tty.usbmodem1411111", "baud_rate": 115200, "timeout": 5},
+    "serial": {"port": "/dev/cu.usbmodem1441111", "baud_rate": 115200, "timeout": 5},
     "filename": "GEM_1playerPilot_",
     "data_dir": "/Users/janatalab/Documents/Arduino/Data", #TODO: change on lab computer
     "hfile": rootpath + "GEM/GEM/GEMConstants.h",
     "slaves_requested": 1,
     "metronome_alpha": [0, 0.25, 0.5, 0.75, 1],
     "metronome_tempo": 120.0, #units: beats-per-minute
-    "repeats": 10, #fairhurst was 12
-    "windows": 26, #number of windows
+    "repeats": 1,#10, #fairhurst was 12
+    "windows": 20, #26, #number of windows
     "audio_feedback": ["hear_metronome"],
     "metronome_heuristic": ["average"]
 }
